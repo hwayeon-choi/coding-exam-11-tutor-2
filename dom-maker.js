95254ddb980d6b4f770ed1e23d5724f3c31c2764
kongukjae <kongukjae@gmail.com>
2022-11-28 15:53:22 +0900

    content 폴더에 있는 '파일 목록'들 리딩하는 작업, 간단하게 콘솔에 찍게 해두었고, 다른 하나는 json 처리 하도록 해놓음

--- dom-maker.js
+++ dom-maker.js
@@ -1,2 +1,14 @@
 const path = require('path');
+const fs = require('fs');
 
+fs.readdir("./content", (err, fileNameElement) => {
+  let data = [];
+  if (err)  throw err; 
+  console.log(fileNameElement);
+  data += fileNameElement.map((fileName) => {
+    return fileName;
+  })
+  const testJSON = JSON.stringify(fileNameElement, null, 2);
+  fs.writeFileSync("./content/test.json", testJSON);
+  console.log(data);
+});
