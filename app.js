254e857a53a9f16453e369dd947f1210d8d2883b
kongukjae <kongukjae@gmail.com>
2022-11-28 15:26:05 +0900

    reqest method 분기처리 해놓기(알맹이만 만들면됨)

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   const getMethod = req.method;
-
   switch (getMethod) {
     case 'GET':
+      res.writeHead(200, {"Content-Type" : "text/html"});
+      res.write();
+      res.end();
     break;
-    
+    case 'POST':
+      console.log('hello post');
+    break;
     default:
+      console.log('hello error');
     break;
-
+  }
 });
