aa1b5da359cc373cb5d78bda88f061c932a2a36b
kongukjae <kongukjae@gmail.com>
2022-11-28 13:07:50 +0900

    복잡하니까 일단 요청 틀만 만들어놓기

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const http = require('http');
-console.dir(http);
+
+const server = http.createServer((req, res) => {
+  const getMethod = req.method;
+
+  switch (getMethod) {
+    case 'GET':
+    break;
+    
+    default:
+    break;
+
+});
